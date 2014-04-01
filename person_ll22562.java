61334a954155359c88dc43814912c5c72fc21e70
365ll22562 <365ll22562test@gmail.com>
2014-04-01 10:33:47 -0500

    finished assignment locally

---
+++ person_ll22562.java
@@ -0,0 +1,10 @@
+public class person_ll22562 {
+//Write a Person class in Java that has a constructor
+//Name your Person class Person_<<YOUREID>>.java
+
+	//added constructor
+	public class person_ll22562 {
+
+	}
+
+}
