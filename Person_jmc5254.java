e0be406388d8a02b3858254a22ecfb8f43957f04
jennxxxd <jenn.chavarria@gmail.com>
2014-04-01 10:33:47 -0500

    finished assignment locally

---
+++ Person_jmc5254.java
@@ -0,0 +1,8 @@
+public class Person_jmc5254 {
+//Write a Person class in Java that has a constructor
+//Name your Person class Person_<<YOUREID>>.java
+
+    // Added constructor.
+    public Person_jmc5254 {
+    }
+}
