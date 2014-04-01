d278b7e2a25c627026f7f6d57d30573076c8b5fe
373student3 <373student3@gmail.com>
2014-04-01 10:33:53 -0500

    finished assignment locally

---
+++ Person_ctd446.java
@@ -0,0 +1,9 @@
+public class Person_ctd446 {
+//Write a Person class in Java that has a constructor
+//Name your Person class Person_<<YOUREID>>.java
+
+	//added constructor
+	public Person_ctd446 {
+
+	}
+}
